97483628df1e003733090b0f9e7133bdc3a4f768
mcclayac <anthony.mcclay@me.com>
2019-03-13 20:07:21 -0500

    chaged the port and the dockerfile for kubernetes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
 FROM node:6.14.2
-EXPOSE 8080
-COPY server.js .
-CMD node server.js
+EXPOSE 9393
+RUN useradd -ms /bin/bash james
+USER james
+#COPY app /app
+WORKDIR /app
+
+#CMD ["python", "app.py"]
+CMD ["node", "server.js"]
+#COPY server.js .
+#CMD node server.js
--- app/server.js
+++ app/server.js
@@ -3,8 +3,8 @@ var http = require('http');
 var handleRequest = function(request, response) {
     console.log('Received request for URL: ' + request.url);
     response.writeHead(200);
-    response.end('Hello World!');
+    response.end('Hello World! \n\n These Nutz ! \n\n nodeservice ');
 };
 var www = http.createServer(handleRequest);
-www.listen(8080);
+www.listen(9393);
 
